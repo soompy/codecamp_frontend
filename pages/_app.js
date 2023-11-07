254940982905d4633dde742cc032554a569eeaec
soomin <sooper1137@gmail.com>
2023-11-07 15:47:09 +0900

    reac의 구조

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
-import '@/styles/globals.css'
+// 모든 페이지에 적용되는 공통 설정
+
+// import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
