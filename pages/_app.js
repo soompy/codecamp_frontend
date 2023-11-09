78cd375a4c4c92375ec7053c4f36f0dce8867155
soomin <sooper1137@gmail.com>
2023-11-09 17:30:08 +0900

    GraphQL 세팅

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 // 모든 페이지에 적용되는 공통 설정
-
 // import '@/styles/globals.css'
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client"
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({ Component }) {
+
+  // 그래프큐엘 세팅
+  const client = new ApolloClient({
+    uri: "http://backend-practice.codebootcamp.co.kr/graphql/",
+    cache: new InMemoryCache() // 컴퓨터의 메모리에다가 백엔드에서 받아온 데이터 임시로 저장해 놓기 => 나중에 더 자세히 알아보기
+  })
+
+  return (
+    <ApolloProvider client={client}>
+      <Component />
+    </ApolloProvider>
+  )
 }
