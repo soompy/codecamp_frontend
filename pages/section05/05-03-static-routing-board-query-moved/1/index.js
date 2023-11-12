85bfc92ebc12c5649ad2104b8945224ae5081389
soomin <sooper1137@gmail.com>
2023-11-12 15:36:34 +0900

    조건부 렌더링: 삼항연산자

--- pages/section05/05-03-static-routing-board-query-moved/1/index.js
+++ pages/section05/05-03-static-routing-board-query-moved/1/index.js
@@ -21,7 +21,7 @@ export default function StaticRoutingMovedPage() {
             <div>1번 게시물로 이동이 완료되었습니다.</div>
             <div>작성자: { data?.fetchBoard.writer }</div>
             <div>제목: { data?.fetchBoard.title }</div>
-            <div>내용: { data?.fetchBoard.contents }</div>            
+            <div>내용: { data ? data.fetchBoard.contents : "로딩중" }</div>            
         </div>
     )
 }
