3f49b85d8306bf28422a97b20f808fdd942ae96d
soomin <sooper1137@gmail.com>
2023-11-12 15:35:13 +0900

    조건부 렌더링: 옵셔널 체이닝(optional-chaining) 작성법

--- pages/section05/05-03-static-routing-board-query-moved/1/index.js
+++ pages/section05/05-03-static-routing-board-query-moved/1/index.js
@@ -19,9 +19,9 @@ export default function StaticRoutingMovedPage() {
     return (
         <div>
             <div>1번 게시물로 이동이 완료되었습니다.</div>
-            <div>작성자: { data && data.fetchBoard.writer }</div>
-            <div>제목: { data && data.fetchBoard.title }</div>
-            <div>내용: { data && data.fetchBoard.contents }</div>            
+            <div>작성자: { data?.fetchBoard.writer }</div>
+            <div>제목: { data?.fetchBoard.title }</div>
+            <div>내용: { data?.fetchBoard.contents }</div>            
         </div>
     )
 }
