61f46708dd9124558f93d61367b77e7865976496
soomin <sooper1137@gmail.com>
2023-11-13 14:42:15 +0900

    컴포넌트 분리 및 파일관리법 container-presenter

---
+++ pages/section06/06-01-container-presenter/index.js
@@ -0,0 +1,13 @@
+import BoardWrite from "@/src/components/units/board/write/BoardWrite.container"
+
+export default function graphqlMutationPage() {
+    
+    // 한 줄일때는 괄호() 필요 없음
+    return (
+        <div>
+            <div>### 여기는 페이지입니다. ###</div>
+            <BoardWrite />
+            <div>### 여기는 페이지입니다. ###</div>
+        </div>
+    )
+}
