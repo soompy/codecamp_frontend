90a4c545a661a10b6d5d5617eaae8f72c9b17dca
soomin <sooper1137@gmail.com>
2023-11-14 17:28:58 +0900

    게시판 글 수정시 기존에 작성했던 default value 유지시키기

---
+++ src/components/units/board/09-write2/BoardWrite.container.js
@@ -0,0 +1,80 @@
+import { useMutation } from "@apollo/client"
+import { useState } from 'react'
+import {나의그래프큐엘셋팅, UPDATE_BOARD} from "./BoardWrite.queries" // export는 골라서 가져오기 가능
+import BoardWriteUI from "./BoardWrite.presenter" // export default로 한개만 가져오기
+import { useRouter } from "next/router"
+/*
+    import ChangeName from "./BoardWrite.presenter" // export default로 이름 바꿔서 가져오기 
+    import ChangeName, { apple } from "./BoardWrite.presenter" // export default와 export 함께 가져오기
+
+    import * as S from "./BoardWrite.styles" // export 한방에 다 가져오기
+    S.BlueButton // export 한방에 다 가져오기
+    S.RedInput // export 한방에 다 가져오기
+*/
+
+export default function BoardWrite(props) {
+    const router = useRouter()
+
+    const [writer, setWriter] = useState()
+    const [title, setTitle] = useState()
+    const [contents, setContents] = useState()
+
+    const [나의함수] = useMutation(나의그래프큐엘셋팅)
+    const [updateBoard] = useMutation(UPDATE_BOARD)
+
+    const onClickSubmit = async () => {
+        // 등록하기
+        const result = await 나의함수({
+            variables: {    // variables 이게 $ 역할을 함
+                writer: writer,
+                title: title,
+                contents: contents
+            }
+        })
+        console.log(result)
+        router.push(`/section09/09-04-boards/${result.data.createBoard.number}`)        
+    }
+
+    const onClickUpdate = async () => {
+        // 수정하기 
+        const result = await updateBoard({
+            variables: {
+                number: Number(router.query.number),
+                writer: writer,
+                title: title,
+                contents: contents
+            }
+        })
+        console.log(result)
+        router.push(`/section09/09-04-boards/${result.date.updateBoard.number}`)
+    }
+
+    const onChangeWriter = (event) => {
+        setWriter(event.target.value)
+    }
+
+    const onChangeTitle = (event) => {
+        setTitle(event.target.value)
+    }
+
+    const onChangeContents = (event) => {
+        setContents(event.target.value)
+    }
+
+    return (
+        <div>
+            <div>$$$$$ 여기는 컨테이너입니다. $$$$$</div>
+            <BoardWriteUI 
+                onClickSubmit={onClickSubmit}
+                onClickUpdate={onClickUpdate}
+                onChangeWriter={onChangeWriter}
+                onChangeTitle={onChangeTitle}
+                onChangeContents={onChangeContents}
+                isEdit={props.isEdit}
+                data={props.data} // undefined이거나, data 이거나 둘중 하나!
+            />
+            {/* <ChangeName /> */}
+            <div>$$$$$ 여기는 컨테이너입니다. $$$$$</div>
+        </div>        
+    )
+}
