9736d9d4f7f268ed1c6a6ba9f6abb10b2fa63b48
soomin <sooper1137@gmail.com>
2023-11-14 12:05:33 +0900

    map으로 boards 만들기

---
+++ pages/section08/08-02-map-boards/index.js
@@ -0,0 +1,40 @@
+import { useQuery, gql } from "@apollo/client"
+
+const FETCH_BOARDS = gql`
+    query{
+        fetchBoards {            
+            writer
+            title
+            contents
+        }
+    }
+`
+
+export default function StaticRoutingMovedPage() {
+    const { data } = useQuery(FETCH_BOARDS)
+    
+    console.log(data?.fetchBoards)
+
+    // const myStyles = {
+    //     margin: "10px",
+    // }
+
+    return (
+        <div>
+            {data?.fetchBoards.map(el => ( 
+                <div>
+                    <span>
+                        <input type="checkbox" />
+                    </span>
+                    <span style={{margin: "10px"}}>{el.writer}</span>
+                    <span style={{margin: "10px"}}>{el.title}</span>
+                    <span style={{margin: "10px"}}>{el.contents}</span>                    
+                </div>                
+            ))}
+            {/* <div>1번 게시물로 이동이 완료되었습니다.</div>
+            <div>작성자: { data?.fetchBoards.writer }</div>
+            <div>제목: { data?.fetchBoards.title }</div>
+            <div>내용: { data ? data.fetchBoards.contents : "로딩중" }</div>             */}
+        </div>
+    )
+}
