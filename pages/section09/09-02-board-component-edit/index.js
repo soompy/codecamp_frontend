723f02e97689db5f47374a1273440fabee15891b
soomin <sooper1137@gmail.com>
2023-11-14 16:25:51 +0900

    컴포넌트 재사용 - api를 받는것을 고려한 효율적인 방법(문자열보다 true/false를 사용하여 표현하는 것이 더 효율적)

--- pages/section09/09-02-board-component-edit/index.js
+++ pages/section09/09-02-board-component-edit/index.js
@@ -1,5 +1,5 @@
 import BoardComponent from "@/src/components/units/board/09-board-component";
 
 export default function BoardEditPage(){
-    return <BoardComponent name="수정" />
+    return <BoardComponent isEdit={true} />
 }
--- src/components/units/board/09-board-component/index.js
+++ src/components/units/board/09-board-component/index.js
@@ -1,10 +1,10 @@
 export default function BoardComponent(props) {    
     return (
         <div>
-            <h1>{props.name}페이지</h1>
+            <h1>{props.isEdit ? "수정" : "등록"}페이지</h1>
             제목: <input type="text" /><br/>
             내용: <input type="text" /><br/>
-            <button>{props.name}하기</button>    
+            <button>{props.isEdit ? "수정" : "등록"}하기</button>    
         </div>        
     )
 }
