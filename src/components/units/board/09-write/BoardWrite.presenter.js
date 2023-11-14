edebde77eaeaa52346dc4446b92c09673f16998e
soomin <sooper1137@gmail.com>
2023-11-14 17:12:36 +0900

    게시판 수정, 등록1

---
+++ src/components/units/board/09-write/BoardWrite.presenter.js
@@ -0,0 +1,21 @@
+import { RedInput, BlueButton } from "./BoardWrite.styles"
+
+export default function BoardWriteUI(props) {
+    
+    return (
+        <div>
+            <div>@@@ 여기는 프리젠터입니다. @@@</div>
+            <div>                
+                작성자: <RedInput type="text" onChange={props.onChangeWriter} />
+                제목: <RedInput type="text" onChange={props.onChangeTitle} />
+                내용: <RedInput type="text" onChange={props.onChangeContents} />
+                <BlueButton onClick={props.isEdit ? props.onClickUpdate : props.onClickSubmit}>
+                    {props.isEdit ? "수정" : "등록"}하기
+                </BlueButton>
+            </div>
+            <div>@@@ 여기는 프리젠터입니다. @@@</div>
+        </div>        
+    )
+}
+
+// export const apple = 3
