3662bb879a34a38db623dfe99d9c9efd8a3dd5a7
이수민 <isoomin@isumin-ui-MacBookPro.local>
2024-06-18 10:25:08 +0900

    emotion으로 UI에 스타일 주기

--- pages/section13/13-01-library-icon/index.tsx
+++ pages/section13/13-01-library-icon/index.tsx
@@ -1,5 +1,11 @@
 import { UpCircleOutlined } from "@ant-design/icons";
+import styled from "@emotion/styled";
+
+const MyIcon = styled(UpCircleOutlined)`
+    color: red;
+    font-size: 50px;
+`;
 
 export default function LibraryIconPage(): JSX.Element {
-    return <UpCircleOutlined />
+    return <MyIcon />;
 }
