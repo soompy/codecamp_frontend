bca3c70f6887fd5b0ab5487fbbe020427abbd21d
이수민 <isoomin@isumin-ui-MacBookPro.local>
2024-06-19 01:05:53 +0900

    star UI 적용하기 2단계 방식

--- pages/section13/13-02-library-star/index.tsx
+++ pages/section13/13-02-library-star/index.tsx
@@ -7,12 +7,16 @@ export default function App(): JSX.Element {
   const [value, setValue] = useState(3);
 
 //   === 1단계 방식 ===
-  const onChangeStar = (value: number): void => {
-    setValue(value);
-  }
+//   const onChangeStar = (value: number): void => {
+//     setValue(value);
+//   }
+
+//   === 2단계 방식 ===
+    const onChangeStar = (value) => setValue(value);
 
   return (
-    <Rate onChange={onChangeStar} value={value} /> // 1단계
+    // <Rate onChange={onChangeStar} value={value} /> // 1단계 방식
+    <Rate onChange={onChangeStar} value={value} /> // 2단계 방식
     //   return <Rate onChange={setValue} value={value} />;
   )
 };
