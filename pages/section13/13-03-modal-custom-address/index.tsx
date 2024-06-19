0bbfb55c0a3a50526253ac49ef52d5b3bdd43977
이수민 <isoomin@isumin-ui-MacBookPro.local>
2024-06-19 14:57:13 +0900

    모달 데이터 초기화 방식

--- pages/section13/13-03-modal-custom-address/index.tsx
+++ pages/section13/13-03-modal-custom-address/index.tsx
@@ -27,23 +27,25 @@ export default function ModalAlertPage(): JSX.Element {
     return (
         <>
             <button onClick={showModal}>모달창 열기!!</button>
-            {/* 모달 종료 방식 - 1. 모달 숨기는 방법 */}
-            <Modal
+            {/* 모달 종료 방식 - 1. 모달 숨기는 방법(ex. 이력서 등) */}
+            {/* <Modal
                 open={isOpen}
                 onOk={handleOk}
                 onCancel={handleCancel}
             >
                 <DaumPostcodeEmbed onComplete={handleComplete}  />
-            </Modal>
+            </Modal> */}
 
-            {/* 모달 종료 방식 - 2. 모달 삭제하는 방법 */}
-            <Modal
-                open={isOpen}
-                onOk={handleOk}
-                onCancel={handleCancel}
-            >
-                <DaumPostcodeEmbed onComplete={handleComplete}  />
-            </Modal>
+            {/* 모달 종료 방식 - 2. 모달 삭제하는 방법(ex. 신용카드, 비밀번호 등) */}
+            {isOpen && (
+                <Modal
+                    open={true}
+                    onOk={handleOk}
+                    onCancel={handleCancel}
+                >
+                    <DaumPostcodeEmbed onComplete={handleComplete}  />
+                </Modal>
+            )}
         </>
     );
 }
