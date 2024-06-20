13859064716bf2235b3fdb4d3dfdf1b86338f994
이수민 <isoomin@isumin-ui-MacBookPro.local>
2024-06-20 18:14:49 +0900

    레이아웃 app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,8 +11,12 @@ export default function App({ Component }: AppProps): JSX.Element {
   });
 
   return (
-    <ApolloProvider client={client}>
-      <Component />
-    </ApolloProvider>
+    <div>
+      <div>==== 여기는 _app.js 컴포넌트 시작 부분 입니다. ====</div>
+        <ApolloProvider client={client}>
+          <Component />
+        </ApolloProvider>
+        <div>==== 여기는 _app.js 컴포넌트 마지막 부분 입니다. ====</div>
+    </div>
   );
 }
