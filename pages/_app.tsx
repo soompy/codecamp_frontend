349749c0735c4c578ac5d30c78064b07a5b9e64f
이수민 <isoomin@isumin-ui-MacBookPro.local>
2024-06-21 17:19:12 +0900

    global 스타일과 폰트 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,22 @@
 // 모든 페이지에 적용되는 공통 설정
 // import '@/styles/globals.css'
+import { Global } from "@emotion/react";
 import { AppProps } from "next/app";
 import Layout from "../src/components/commons/layout";
 import ApolloSetting from "../src/components/commons/apollo";
+import { globalStyles } from "../src/commons/styles/globalStyles";
 
 export default function App({ Component }: AppProps): JSX.Element {
-  
-
   return (
     <div>
       <div>==== 여기는 _app.js 컴포넌트 시작 부분 입니다. ====</div>
         <ApolloSetting>
+          <>
+          <Global styles={globalStyles} />
           <Layout>
             <Component />
           </Layout>
+          </>
         </ApolloSetting>
         <div>==== 여기는 _app.js 컴포넌트 마지막 부분 입니다. ====</div>
     </div>
