9dc2d753468df4009d7631c53b86a7c7aa5fdceb
이수민 <isoomin@isumin-ui-MacBookPro.local>
2024-06-21 16:32:44 +0900

    레이아웃 조건부 렌더링 - 특정 화면에서 헤더 노출하지 않도록 만들기

--- src/components/commons/layout/index.tsx
+++ src/components/commons/layout/index.tsx
@@ -1,16 +1,30 @@
+import { useRouter } from "next/router";
 import LayoutBanner from "./banner";
 import LayoutFooter from "./footer";
 import LayoutHeader from "./header";
 import LayoutNavigation from "./navigation";
+
+// 조건부 렌더링
+const HIDDEN_HEADERS = [
+    "/section13/13-01-library-icon",
+    "/section13/13-02-library-star",
+];
 
 interface ILayoutProps {
     children: JSX.Element;
 }
 
 export default function Layout(props: ILayoutProps): JSX.Element {
+    const router = useRouter()
+    console.log("===========")
+    console.log(router.asPath)
+    console.log("===========")
+
+    const isHiddenHeader = HIDDEN_HEADERS.includes(router.asPath);
+
     return (
         <>
-            <LayoutHeader />
+            {!isHiddenHeader && <LayoutHeader />}            
             <LayoutBanner />
             <LayoutNavigation />
             <div style={{ height: "500px", display: "flex " }}>
