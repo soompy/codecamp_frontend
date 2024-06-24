d38bc4ef5ea633545b2fc7a6f944b7b647702a1a
이수민 <isoomin@isumin-ui-MacBookPro.local>
2024-06-24 17:21:59 +0900

    state 끌어 올리기 활용 예

--- pages/section15/15-04-lifting-state-up/index.tsx
+++ pages/section15/15-04-lifting-state-up/index.tsx
@@ -1,19 +1,21 @@
-import { useState } from "react"
-import Child1 from "../../../src/components/units/15-lifting-state-up/Child1"
-import Child2 from "../../../src/components/units/15-lifting-state-up/Child2"
+import { useState } from "react";
+import Child1 from "../../../src/components/units/15-lifting-state-up/Child1";
+import Child2 from "../../../src/components/units/15-lifting-state-up/Child2";
 
 export default function CounterLetDocumentPage(): JSX.Element {
-    const [ count, setCount ] = useState(0);
+  const [count, setCount] = useState(0);
 
-    const onClickCount = (): void => {
-      setCount((prev) => prev + 1);
-    };
+  const onClickCount = (): void => {
+    setCount((prev) => prev + 1);
+  };
 
-    return (
-        <div>
-            <Child1 count={count} setCount={setCount} />
-            <div>=========================</div>
-            <Child2 count={count} onClickCount={onClickCount} />
-        </div>        
-    )
+  return (
+    <div>
+      {/* 게시글 목록 */}
+      <Child1 count={count} setCount={setCount} />
+      <div>=========================</div>
+      {/* 페이지네이션 */}
+      <Child2 count={count} onClickCount={onClickCount} />
+    </div>
+  );
 }
