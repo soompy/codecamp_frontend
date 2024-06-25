1f2bfa884325b880ae20802843fec13bf90d5273
이수민 <isoomin@isumin-ui-MacBookPro.local>
2024-06-25 15:39:21 +0900

    comment 여러개 수정 가능하도록 만들기, 스프레드 연산자와 데이터 복사

--- pages/section16/16-01-comment-edit-2/index.tsx
+++ pages/section16/16-01-comment-edit-2/index.tsx
@@ -18,20 +18,34 @@ const FETCH_BOARDS = gql`
 `;
 
 export default function StaticRoutingMovedPage(): JSX.Element {
-  const [myIndex, setMyIndex] = useState(-1);
+  const [myIndex, setMyIndex] = useState([
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+  ]);
 
   const { data } = useQuery<Pick<IQuery, "fetchBoards">, IQueryFetchBoardsArgs>(
     FETCH_BOARDS,
   );
 
   const onClickEdit = (event: MouseEvent<HTMLButtonElement>): void => {
-    setMyIndex(Number(event.currentTarget.id));
+    const qqq = [...myIndex];
+    qqq[Number(event.currentTarget.id)] = true;
+    console.log(qqq)
+    setMyIndex(qqq);
   };
 
   return (
     <div>
       {data?.fetchBoards.map((el, index) =>
-        index !== myIndex ? (
+        !myIndex[index] ? (
           <div key={el._id}>
             <span style={{ margin: "10px" }}>{el.title}</span>
             <span style={{ margin: "10px" }}>{el.writer}</span>
