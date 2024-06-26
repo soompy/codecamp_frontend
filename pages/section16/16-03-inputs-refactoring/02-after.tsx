27999f0711d095bfd1c8559141fbc9bbfc8da262
이수민 <isoomin@isumin-ui-MacBookPro.local>
2024-06-26 11:53:41 +0900

    section 20 클래스 함수 상속관계 확인해보기

--- pages/section16/16-03-inputs-refactoring/02-after.tsx
+++ pages/section16/16-03-inputs-refactoring/02-after.tsx
@@ -19,14 +19,8 @@ export default function graphqlMutationPage() {
         contents: ""
     })
     
-    const [나의함수] = useMutation(나의그래프큐엘셋팅)
 
-    const onClickSubmit = async () => {
-        const result = await 나의함수({
-            variables: { ...inputs },
-        });
-        console.log(result)
-    }
+  
 
     const onChangeInputs = (event) => {
         setInputs((prev) => {            
@@ -43,7 +37,7 @@ export default function graphqlMutationPage() {
             작성자: <input type="text" id="writer" onChange={onChangeInputs} />
             제목: <input type="text" id="title" onChange={onChangeInputs} />
             내용: <input type="text" id="contents" onChange={onChangeInputs} />
-            <button onClick={onClickSubmit}>GRAPHQL-API 요청하기</button>
+            
         </div>        
     ) 
 }
