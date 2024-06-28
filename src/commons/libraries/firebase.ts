5369d58630a3660272cce9c8eae8310dbd63f756
이수민 <isoomin@isumin-ui-MacBookPro.local>
2024-06-28 23:53:47 +0900

    section18 firebase로 데이터 등록, 조회하기

---
+++ src/commons/libraries/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDPDyjwP8R3_35wcx9FkOa75fMjUp0IBaU",
+  authDomain: "myfirebase-57dd1.firebaseapp.com",
+  projectId: "myfirebase-57dd1",
+  storageBucket: "myfirebase-57dd1.appspot.com",
+  messagingSenderId: "268891229690",
+  appId: "1:268891229690:web:5063fa3dfb7aecf8966843"
+};
+
+// Initialize Firebase
+export const firebaseApp = initializeApp(firebaseConfig);
