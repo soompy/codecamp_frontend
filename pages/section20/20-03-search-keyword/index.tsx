40252c1f31abfee1388d431d031bc0a8cb5a7dcd
이수민 <sooper1137@gmail.com>
2024-07-06 11:25:17 +0900

    검색 keyword

---
+++ pages/section20/20-03-search-keyword/index.tsx
@@ -0,0 +1,81 @@
+import { useQuery, gql } from "@apollo/client";
+import { useState } from "react";
+import type {
+  IQuery,
+  IQueryFetchBoardsArgs,
+} from "../../../src/commons/types/generated/types";
+import type { MouseEvent, ChangeEvent } from "react";
+import _ from "lodash";
+import { v4 as uuidv4 } from "uuid";
+
+const FETCH_BOARDS = gql`
+  query fetchBoards($page: Int, $search: String) {
+    fetchBoards(page: $page, search: $search) {
+      _id
+      writer
+      title
+      contents
+    }
+  }
+`;
+
+export default function StaticRoutingMovedPage(): JSX.Element {
+  // const [search, setSearch] = useState("");
+
+  // 키워드 저장할 state 만들기
+  const [keyword, setKeyword] = useState("");
+
+  const { data, refetch } = useQuery<
+    Pick<IQuery, "fetchBoards">,
+    IQueryFetchBoardsArgs
+  >(FETCH_BOARDS);
+
+  const onClickPage = (event: MouseEvent<HTMLSpanElement>): void => {
+    // 검색에서 refetch할 때, search 검색어가 refetch에 이미 저장되어 있는 상태이므로 추가로 search 포함하지 않아도 됨
+    void refetch({ page: Number(event.currentTarget.id) });
+  };
+
+  const getDebounce = _.debounce((value) => {
+    void refetch({ search: value, page: 1 });
+    setKeyword(value);
+  }, 500);
+
+  const onchangeSearch = (event: ChangeEvent<HTMLInputElement>): void => {
+    // setSearch(event.currentTarget.value);
+    getDebounce(event.currentTarget.value);
+  };
+
+  //   const onClickSearch = (): void => {
+  //     void refetch({ search, page: 1 });
+  //   };
+
+  return (
+    <div>
+      검색어 입력: <input type="text" onChange={onchangeSearch} />
+      {/* <button onClick={onClickSearch}>검색하기</button> */}
+      {data?.fetchBoards.map((el) => (
+        <div key={el._id}>
+          <span style={{ margin: "10px" }}>
+            {el.title.split(keyword).map((part, index, arr) => (
+              <span
+                key={uuidv4()}
+                style={{ color: part === keyword ? "red" : "black" }}
+              >
+                {part}
+                {index < arr.length - 1 && (
+                  <span style={{ color: "red" }}>{keyword}</span>
+                )}
+              </span>
+            ))}
+          </span>
+          <span style={{ margin: "10px" }}>{el.writer}</span>
+        </div>
+      ))}
+      {new Array(10).fill(1).map((_, index) => (
+        <span key={index + 1} id={String(index + 1)} onClick={onClickPage}>
+          {index + 1}
+        </span>
+      ))}
+    </div>
+  );
+}
