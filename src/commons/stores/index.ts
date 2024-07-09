f39e3b1b84189bc7c7da70c9ea6f5fe5ff3cc208
이수민 <sooper1137@gmail.com>
2024-07-09 18:10:56 +0900

    로그인 accessToken (토큰)

--- src/commons/stores/index.ts
+++ src/commons/stores/index.ts
@@ -1,6 +1,11 @@
 import { atom } from "recoil";
 
 export const isEditState = atom({
-    key: "isEditState",
-    default: true,
-})
+  key: "isEditState",
+  default: true,
+});
+
+export const accessTokenState = atom({
+    key: "accessTokenState",
+    default: "",
+});
