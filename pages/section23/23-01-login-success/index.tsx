b692fe168a14160321654999bcd27a44661d972b
이수민 <sooper1137@gmail.com>
2024-07-09 17:12:34 +0900

    section26 로그인, 로그인 성공

---
+++ pages/section23/23-01-login-success/index.tsx
@@ -0,0 +1,3 @@
+export default function LoginPage(): JSX.Element {
+  return <>철수님 환영합니다!</>;
+}
---
+++ pages/section23/23-01-login/index.tsx
@@ -0,0 +1,9 @@
+export default function LoginPage(): JSX.Element {
+  return (
+    <>
+      이메일: <input type="text" />
+      비밀번호 : <input type="password" />
+      <button>로그인</button>
+    </>
+  );
+}
